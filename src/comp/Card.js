d6d1c880a820df68144e106e60cd193eda1ef699
Abass Ben Cheick <cheikabassben@gmail.com>
2022-02-11 19:41:38 +0000

    initial commit

---
+++ src/comp/Card.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {Linking, TouchableOpacity, ImageBackground, Dimensions, StyleSheet, View, Text} from 'react-native'
+import Ionicons from '@expo/vector-icons/Ionicons'
+
+export default class Card extends Component {
+    
+    render() {
+        return (
+            <View>
+                <Text>Card</Text>
+            </View>
+        )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,1 @@
+export {default as Dynacard} from './comp/Card'
